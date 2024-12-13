262ab0f84d21f98dcceba826935e5d9a7a03196c
chrisz118 <zikoc118@gmail.com>
2024-12-13 15:27:56 -0500

    Connected user profile to navigation bar

--- app/src/main/java/com/example/checkball/MainActivity.kt
+++ app/src/main/java/com/example/checkball/MainActivity.kt
@@ -8,15 +8,17 @@ import androidx.navigation.compose.*
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
-import com.example.checkball.ui.BottomNavigationBar
 import com.example.checkball.ui.screen.LoginScreen
 import com.example.checkball.ui.screen.SignUpScreen
 import com.example.checkball.ui.screen.MainScreen
 import com.example.checkball.ui.screen.HistoryScreen
 import com.example.checkball.ui.screen.HighlightsScreen
-import com.example.checkball.ui.screen.ProfileScreen
+import com.example.checkball.ui.screen.UserProfileScreen
+import com.example.checkball.viewmodel.UserProfileViewModel
+import com.example.checkball.ui.BottomNavigationBar
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -34,13 +36,15 @@ fun AppNavigation() {
     val auth = FirebaseAuth.getInstance()
     val isLoggedIn = auth.currentUser != null
     val startDestination = if (isLoggedIn) "main" else "login"
+    val userId = auth.currentUser?.uid
+    val userProfileViewModel: UserProfileViewModel = viewModel()
 
-    Scaffold(
-//        bottomBar = {
-//            if (isLoggedIn) {
-//                BottomNavigationBar(navController)
-//            }
-//        }
+    Scaffold (
+        bottomBar = {
+            if (isLoggedIn) {
+                BottomNavigationBar(navController)
+            }
+        }
     ) { innerPadding ->
         NavHost(
             navController = navController,
@@ -53,7 +57,17 @@ fun AppNavigation() {
             composable("main") { MainScreen(navController) }
             composable("gameDetails") { HistoryScreen() }
             composable("communityFeed") { HighlightsScreen() }
-            composable("profile") { ProfileScreen() }
+            composable("profile") {
+                userId?.let {
+                    UserProfileScreen(
+                        onViewMatchHistoryClick = { navController.navigate("match_history_screen") },
+                        userProfileViewModel = userProfileViewModel,
+                        userID = it,
+                        onSaveProfile = { /* Placeholder lambda for on-save functionality */ }
+
+                    )
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/checkball/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/checkball/ui/screen/MainScreen.kt
@@ -16,7 +16,6 @@ import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.maps.android.compose.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
-import com.example.checkball.ui.BottomNavigationBar
 import com.example.checkball.viewmodel.AuthViewModel
 import com.example.checkball.viewmodel.MapViewModel
 import com.example.checkball.viewmodel.Place
@@ -100,9 +99,6 @@ fun MainScreen(navController: NavHostController) {
                         }
                     }
                 )
-            },
-            bottomBar = {
-                BottomNavigationBar(navController = navController)
             },
             content = { padding ->
                 Column(
@@ -266,8 +262,3 @@ fun CourtDetailCard(
         }
     }
 }
-
-
-
-
-
